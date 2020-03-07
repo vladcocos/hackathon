9db50ceec1dab1f090445fb55ededdc094e94782
Alexandru Stroie <andistroie@yahoo.com>
2020-03-07 16:16:51 +0200

    added menu

---
+++ utils.py
@@ -0,0 +1,8 @@
+avira_product = ['Avira Prime', 'Antivirus PRO', 'Phantom VPN', 'Password Manager', 'Optimizer', 'System Speedup']
+
+avira_product_prices = {'Avira Prime' : 75,
+						'Antivirus PRO' : 35,
+						'Phantom VPN' : 50, 
+						'Password Manager' : 20,
+						'Optimizer' : 10,
+						'System Speedup' : 25}
